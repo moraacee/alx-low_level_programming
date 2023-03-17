55451515b460fe39f42b910339b87bcb9509e93e
moraacee <moraanancy511@gmail.com>
2023-03-17 09:52:53 -0700

    4-puts.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,12 @@
 #include <stdio.h> 
-
 /**
  * main - Entry point of the program
  *
  * Return: Always 0 (success)
- */ 
-
-int main (void) 
+ */
+int main (void)
 
 { 
-
 	printf("%s%s%s%s", "with proper grammar, but the outcome is a piece of art", ",", " ", "\n"); 
 
 	return(0);
