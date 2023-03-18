89acb42979f35a3e84d7b299e2bd300560e9155a
moraacee <moraanancy511@gmail.com>
2023-03-18 07:11:20 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return : Always 0 (Success)
+ *
+ * puts - write string to the output stream without the null charater
  */
-int main(void) 
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle"); 
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
 
-	return(0); 
-	  
-}  
- 
-	
+
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{ 
+	printf("Size of char:%d bytes(s)\n", sizeof(char));
+	printf("Size of int:%d bytes(s)\n", sizeof(int));
+	printf("Size of a long int:%d bytes(s)\n", sizeof(long int));
+	ptintf("Size of long long int:%d bytes\n", sizeof(long long int));
+	printf("Size of a float:%d bytes\n", sizeof(float));	
+	return (0) 
+
