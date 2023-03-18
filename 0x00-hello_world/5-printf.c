310276f59c5268081afa7a9a7fa644949dba416a
moraacee <moraanancy511@gmail.com>
2023-03-18 16:42:16 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,12 +3,12 @@
 /**
  * main - Entry point
  *
- * Return - Always 0(Success)
+ * Return - Always 0 (Success)
  *
  * printf - prints output to a file followed by a new line
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
+	printf("with proper grammar, but the outcome is a piece of art, \n");
 	return (0);
 }
