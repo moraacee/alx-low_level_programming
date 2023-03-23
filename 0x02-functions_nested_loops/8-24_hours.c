f0dc6c4d0d50e650f92ea612992470730bbf07c5
moraacee <moraanancy511@gmail.com>
2023-03-23 14:33:55 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,8 @@
 #include "main.h"
+
 /**
- * Description- prints every minute of the day of Jack Bauer
- * starting from 00.00 to 23.59
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * starting from 00:00 to 23:59
  */
 void jack_bauer(void)
 {
