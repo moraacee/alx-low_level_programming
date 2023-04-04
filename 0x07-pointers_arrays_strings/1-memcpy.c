4df2a1337411e119270d156cc0cf5e9629d1223c
moraacee <moraanancy511@gmail.com>
2023-04-04 10:02:13 -0700

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,23 +1,21 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
- *_memcpy - a function that copies memory area
- *@dest: memory where is stored
- *@src: memory from where is copied
- *@n: number of bytes
- *
- *Return: A pointer to dest
- */
+*_memcpy - a function that copies memory area
+*@dest: memory where copied information is stored
+*@src: memory from where information is copied
+*@n: number of bytes to be copied
+*
+*Return: A pointer to dest
+*/
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int a = 0;
 	int i = n;
 
-	for (; a < i; a++)
+	for (int i = 0; i < n; i++)
 	{
-		dest[a] = src[a];
-		n--;
+		*(src + i) = *(dest + i);
 	}
 	return (dest);
 }
-
-
--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,9 +1,11 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
- * print_chessboard - Entry point
- * @a: array
- * Return: Always 0 (Success)
- */
+* print_chessboard - Entry point
+* @a: An Array
+* Return: Always 0 (Success)
+*/
 void print_chessboard(char (*a)[8])
 {
 	int i;
@@ -12,7 +14,7 @@ void print_chessboard(char (*a)[8])
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
-			_putchar(a[i][j]);
-		_putchar('\n');
+			putchar(a[i][j]);
+		putchar ('\n');
 	}
 }
