3ead156aea793a9c09250dca5af7cef479247160
moraacee <moraanancy511@gmail.com>
2023-04-05 14:14:26 -0700

    3-factorial.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,12 +9,12 @@
  */
 int _strlen_recursion(char *s)
 {
-	int i = 0;
+	int longit = 0;
 
 	if (*s)
 	{
-		i++;
-		i += _strlen_recursion(s + 1);
+		longit++;
+		longit+= _strlen_recursion(s + 1);
 	}
 
 	return (i);
